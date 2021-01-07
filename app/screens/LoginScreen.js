a44f35aac3e19b75e3b04cfbdaf832294b6a8626
olga <olgshestakova@gmail.com>
2021-01-07 20:26:56 +0100

    create login screen

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+import Screen from "../components/Screen";
+import colors from "../config/colors";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
+import AppText from "../components/AppText";
+import ErrorMessage from "../components/ErrorMessage";
+import AppFormField from "../components/AppFormField";
+import AppForm from "../components/AppForm";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+export default function LoginScreen() {
+  return (
+    <Screen style={styles.screen}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <AppForm
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.warn(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          name={"email"}
+          icon="email"
+          placeholder="Email"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType="password"
+          name={"password"}
+          icon="lock"
+          placeholder="Password"
+        />
+        <AppButton title="Login" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
