1d6d0d7a8fc5d385d2687234d7f85a32eface7b3
olga <olgshestakova@gmail.com>
2021-01-18 17:05:16 +0100

    create Image picker form

---
+++ app/components/ImageInput.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+
+import colors from "../config/colors";
+
+export default function ImageInput({ imageUri, onChangeImage }) {
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) alert("You need to enable permissions");
+  };
+
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert("Delete", "Are you sure want to delete this image?", [
+        {
+          text: "Yes",
+          onPress: () => onChangeImage(null),
+        },
+        { text: "No" },
+      ]);
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.cancelled) onChangeImage(result.uri);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  return (
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            name="camera"
+            size={40}
+            color={colors.medium}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 100,
+    width: 100,
+    overflow: "hidden",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
